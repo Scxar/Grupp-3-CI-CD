21a3e7646255d10ad770551c0dbb88f210983ae1
Alikh-2617 <Alikhavari2617@outlook.com>
2024-01-12 14:25:03 +0100

    kode

---
+++ Grupp-3-CI-CD/Opersion.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grupp_3_CI_CD
+{
+    public class Opersion
+    {
+        public int personmmer(string personnummer)
+        {
+            //  // Ali 
+            // // om personnummer varit 12 sif så det tar bort första 19 
+            // // person nummert ska vara 10 sif
+            return 11;
+        }
+
+        public bool checkPersonNummer(string personnummer)
+        {
+            // // mikael 
+            // // kollar om person nummert är korekt eller inte
+            return true;
+        }
+
+        public string Gender(string personmmer)
+        {
+            //     // säga till om person nummert är män eller kvinna ! 
+
+            return " ";
+        }
+    }
+}
---
+++ Grupp-3-CI-CD/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Grupp_3_CI_CD
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Opersion op = new Opersion();
+            // rätta person nummert 
+            int personnummer = op.personmmer("sdsd");
+            Console.WriteLine(personnummer);
+        }
+    }
+}
