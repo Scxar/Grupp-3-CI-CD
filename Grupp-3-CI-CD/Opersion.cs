d7d58d8de5c3e24c4eb7e0d545cf2d7ba6074420
Alikh-2617 <Alikhavari2617@outlook.com>
2024-01-14 17:19:11 +0100

    last update

--- Grupp-3-CI-CD/Opersion.cs
+++ Grupp-3-CI-CD/Opersion.cs
@@ -10,9 +10,8 @@ namespace Grupp_3_CI_CD
     {
         public long personmmerToInt(string personnummer)
         {
-            //  // Ali 
-            // // om personnummer varit 12 sif så det tar bort första 19 
-            // // person nummert ska vara 10 sif
+            // om personnummer varit 12 sif så det tar bort första 19 
+            // person nummert ska vara 10 sif
             long result = 0;
             personnummer = personnummer.Replace("-","");
             if(personnummer.Length > 10)
--- Grupp-3-CI-CD/xunit_test.cs
+++ Grupp-3-CI-CD/xunit_test.cs
@@ -13,7 +13,26 @@ namespace Grupp_3_CI_CD
             long result1 = 9004263597;
 
             Assert.Equal(result1,opersion.personmmerToInt(pers_nr));
+        }
 
+        [Fact]
+        public void TestName()
+        {
+            // Given
+        
+            // When
+        
+            // Then
+        }
+
+        [Fact]
+        public void TestName2()
+        {
+            // Given
+        
+            // When
+        
+            // Then
         }
 
 
